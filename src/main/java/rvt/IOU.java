a8edab091f1327b1b8b4519d9dee9802085f7307
24DP4JJurc <A240102JJ@rvt.lv>
2026-03-24 10:03:02 +0200

    I owe you java-mooc 8-2

---
+++ src/main/java/rvt/IOU.java
@@ -0,0 +1,37 @@
+package rvt;
+
+import java.util.HashMap;
+
+public class IOU {
+
+    private HashMap<String, Double> debts;
+
+    public IOU() {
+        this.debts = new HashMap<>();
+    }
+
+
+    public void setSum(String toWhom, double amount) {
+        debts.put(toWhom, amount); 
+    }
+
+
+    public double howMuchDoIOweTo(String toWhom) {
+        return debts.getOrDefault(toWhom, 0.0);
+    }
+
+ 
+    public static void main(String[] args) {
+        IOU mattsIOU = new IOU();
+
+        mattsIOU.setSum("Arthur", 51.5);
+        mattsIOU.setSum("Michael", 30);
+
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+        System.out.println(mattsIOU.howMuchDoIOweTo("Michael"));
+
+       
+        mattsIOU.setSum("Arthur", 10.5);
+        System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
+    }
+}
